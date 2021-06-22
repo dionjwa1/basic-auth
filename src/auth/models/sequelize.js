20204a9ab6df241b878ac4c858ef5047ef115874
dionjwa1 <dionjwa@gmail.com>
2021-06-21 17:30:11 -0700

    lab06

---
+++ src/auth/models/sequelize.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// 3rd Party Resources
+const express = require('express');
+const bcrypt = require('bcrypt');
+const base64 = require('base-64');
+const { Sequelize, DataTypes } = require('sequelize');
+
+// Prepare the express app
+const app = express();
+
+// Process JSON input and put the data on req.body
+app.use(express.json());
+
+const sequelize = new Sequelize(process.env.DATABASE_URL);
+
+// Process FORM intput and put the data on req.body
+app.use(express.urlencoded({ extended: true }));
+
+// Create a Sequelize model
+const Users = sequelize.define('User', {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
+});
